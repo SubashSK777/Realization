519978c54a756dc3e5d97022903d4c14317a5950
SubashSK777 <subashsk11831@gmail.com>
2025-06-11 09:31:33 +0530

    rty

---
+++ Remove_Dupli.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Remove_Dupli {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array = [0] * n;
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt;
+        }
+        
+    } 
+}
